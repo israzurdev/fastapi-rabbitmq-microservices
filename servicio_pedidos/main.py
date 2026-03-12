e1bcac85e298418972ee8881522e0d5df015c4f4
israzurdev <misk0z@proton.me>
2026-03-12 21:50:37 +0100

    feat: implementada Fase 3 (seguridad con API Key) y docs actualizados.

--- servicio_pedidos/main.py
+++ servicio_pedidos/main.py
@@ -1,17 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, Security
+from fastapi.security.api_key import APIKeyHeader
 from pydantic import BaseModel
 import pika
 import json
 import os
 from dotenv import load_dotenv, find_dotenv
 
-# Cargamos las variables de entorno
+#Cargamos las variables
 load_dotenv(find_dotenv())
 
-# Credenciales seguras (si no hay .env, usa "guest" por defecto para no exponer claves)
+#Credenciales de RabbitMQ
 RABBIT_USER = os.getenv("RABBIT_USER", "guest")
 RABBIT_PASS = os.getenv("RABBIT_PASS", "guest")
 RABBIT_HOST = os.getenv("RABBIT_HOST", "rabbitmq")
+
+#Configuración de seguridad 
+API_KEY_NAME = "X-Api-Key"
+API_KEY_SECRETA = os.getenv("API_SECRET_KEY", "clave_por_defecto_insegura")
+
+#Le decimos a FastAPI que busque la clave en las cabeceras (Headers) de la petición
+api_key_header =  APIKeyHeader(name=API_KEY_NAME, auto_error=False)
+
+async def verificar_api_key(api_key: str = Security(api_key_header)):
+    if api_key == API_KEY_SECRETA:
+        return api_key
+    #Si no coincide o no existe, lanza error 403(Forbidden)
+    raise HTTPException(status_code=403, detail="Acceso denegado: API Key inválida o ausente.")
 
 app = FastAPI(title="API de Pedidos")
 
@@ -19,26 +33,21 @@ class Pedido(BaseModel):
     producto: str
     cantidad: int
     email_cliente: str
-
+    
+#Ahora pasamos 'api_key: str = (verificar_api_key)' al endpoint
 @app.post("/pedidos")
-async def crear_pedido(pedido: Pedido):
-    # Conectamos con el Broker RabbitMQ
+async def crear_pedido(pedido: Pedido, api_key: str = Depends(verificar_api_key)):
     credenciales = pika.PlainCredentials(RABBIT_USER, RABBIT_PASS)
     conexion = pika.BlockingConnection(pika.ConnectionParameters(host=RABBIT_HOST, credentials=credenciales))
     canal = conexion.channel()
-
-    # Aseguramos que la cola existe
+    
     canal.queue_declare(queue='cola_notificaciones')
     
-    # Preparamos el mensaje
     mensaje = json.dumps(pedido.model_dump())
-    
-    # Publicamos el mensaje en la cola
     canal.basic_publish(
         exchange='',
         routing_key='cola_notificaciones',
         body=mensaje
     )
-    
     conexion.close()
-    return {"status": "ok", "mensaje": "Pedido encolado correctamente y listo para procesar"}
+    return{"status": "ok", "mensaje": "Pedido recibido y validado. Encolado correctamente."}
